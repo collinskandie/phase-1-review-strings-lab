4821d77dca516264ea93f3736df57dd1d14e45de
Collins Kandie <collinskandie1@gmail.com>
2024-06-26 21:59:27 +0300

    all test passed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'John Doe';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
